ff71703325585cb09afeb7a6dcd0ea7c07215cb6
AbdullohMurodilloyev <abdullohmurodilloyev98@gmail.com>
2025-02-26 14:42:47 +0500

    create project

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OsonTask
+//
+//  Created by Abdulloh Murodilloyev on 26/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
